393054c702a4288c698ff754138fb6f43c0b0db2
Jose Eneas da Silva Maria <joseeneas@me.com>
2024-05-03 13:05:14 -0400

    General sync

---
+++ DSA/c/fibonacci_find.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int F(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        return F(n - 1) + F(n - 2);
+    }
+}
+
+int main()
+{
+    printf("%d\n", F(19));
+    return 0;
+}
+
+// C
